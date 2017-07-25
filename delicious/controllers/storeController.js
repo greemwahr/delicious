6145fc540da47669df507254830ef4995d8631af
shadowsyntax <tibetianwolf@gmail.com>
2017-07-25 21:38:09 +0100

    General house cleaning ;)

--- delicious/controllers/storeController.js
+++ delicious/controllers/storeController.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const Store = mongoose.model('Store')
 
 exports.homePage = (req, res) => {
   console.log(req.name)
@@ -10,5 +10,8 @@ exports.addStore = (req, res) => {
   res.render('editStore', {title: 'Add Store'})
 }
 
-exports.createStore = (req, res) => {
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body)
+  await store.save()
+  res.redirect('/')
 }
