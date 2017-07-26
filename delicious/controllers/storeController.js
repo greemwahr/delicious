0fb17fcbec3890572ad089823a8376cefde16c79
shadowsyntax <tibetianwolf@gmail.com>
2017-07-26 20:43:45 +0100

    End of Control Flow module

--- delicious/controllers/storeController.js
+++ delicious/controllers/storeController.js
@@ -11,7 +11,33 @@ exports.addStore = (req, res) => {
 }
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body)
-  await store.save()
-  res.redirect('/')
+  const store = await (new Store(req.body)).save()
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review`)
+  res.redirect(`/store/${store.slug}`)
+}
+
+exports.getStore = async (req, res) => {
+  // 1. Query the database for a list of all stores
+  const stores = await Store.find()
+  res.render('stores', { title: 'Stores', stores })
+}
+
+exports.editStore = async (req, res) => {
+  // 1. Find the store given the ID
+  const store = await Store.findOne({ _id: req.params.id })
+  // 2. Confirm they are the owner of the store
+  //TODO
+  // 3. Render out the edit form so that the user can update the store
+  res.render('editStore', { title: `Edit ${store.name}`, store } )
+}
+
+exports.updateStore = async (req, res) => {
+  //find and update the store
+  const store = Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true
+  }).exec()
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store -></a>`)
+  res.redirect(`/stores/${store._id}/edit`)
+  // Redirect them to the new store and tell them it worked
 }
